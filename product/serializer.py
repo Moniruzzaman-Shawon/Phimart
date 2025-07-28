74c33342564e0b10a409bdf905ebee6555c9786b
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-07-28 08:21:30 +0600

    categories features implemented

--- product/serializer.py
+++ product/serializer.py
@@ -1,13 +1,45 @@
 from rest_framework import serializers
 from decimal import Decimal
+from product.models import Category, Product
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, source = 'price') #source for identify, now I can change 'price_name'
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'product_count']    
+
+    product_count = serializers.IntegerField()
+
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     name = serializers.CharField()
+#     unit_price = serializers.DecimalField(max_digits=10, decimal_places=2, source = 'price') #source for identify, now I can change 'price_name'
+
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     # category = serializers.PrimaryKeyRelatedField(
+#     #     queryset = Category.objects.all()
+#     # )
+#     # category = serializers.StringRelatedField()
+#     # category = CategorySerializer()
+#     category = serializers.HyperlinkedRelatedField(
+#         queryset = Category.objects.all(),
+#         view_name =  'view-specific-category'
+#     )
+
+#     def calculate_tax(self,product):
+#         return round(product.price * Decimal(1.1), 2)
+    
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        # ---- fields = '__all__' # to show all fields (but not recommended)
+        fields = ["id", "name", 'description', 'price', 'stock', 'category', 'price_with_tax']
 
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    category = serializers.HyperlinkedRelatedField(
+    queryset = Category.objects.all(),
+        view_name =  'view-specific-category')
+    def calculate_tax(self, product):
+        return round(product.price * Decimal(1.1) , 2)
 
 
-    def calculate_tax(self,product):
-        return round(product.price * Decimal(1.1), 2)
--- product/views.py
+++ product/views.py
@@ -4,17 +4,33 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product, Category
-from product.serializer import ProductSerializer
+from product.serializer import ProductSerializer, CategorySerializer
+from django.db.models import Count
 
 # Create your views here.
+@api_view()
+def view_products(request):
+    products = Product.objects.select_related('category').all()
+    serializer = ProductSerializer(products, many=True, context={'request': request})
+    return Response(serializer.data)
+
 @api_view() # to set response as api view
 def view_specific_product(request, id):
-        product = get_object_or_404(Product, pk=id)
-        product = Product.objects.get(pk=id)
-        serilizer = ProductSerializer(product)
-        return Response(serilizer.data)
-    
+    product = get_object_or_404(Product, pk=id)
+    product = Product.objects.get(pk=id)
+    serilizer = ProductSerializer(product)
+    return Response(serilizer.data)
+
 
 @api_view() 
 def view_categories(request):
-    return Response({"message": "Categories"})
+    categories = Category.objects.annotate(product_count = Count('products')).all()
+    serializer = CategorySerializer(categories, many= True)
+    return Response(serializer.data)
+
+
+@api_view()
+def view_specific_category(request, pk):
+    category = get_object_or_404(Category, pk = pk)
+    serilizer = CategorySerializer(category)
+    return Response(serilizer.data)
