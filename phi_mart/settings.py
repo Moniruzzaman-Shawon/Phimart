0bbd3e7fc1e08e99050c63626b46243d77bc428e
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-08-24 12:57:34 +0600

    cors header added

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles', 
     'drf_yasg',  
+    "corsheaders",
     'rest_framework',
     'djoser',
     'api',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -70,6 +72,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'phi_mart.wsgi.app'
+
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:5173",
+    
+]
 
 INTERNAL_IPS = [
     # ...
