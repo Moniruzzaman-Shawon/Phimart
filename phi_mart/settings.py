5bf179aab68925f5c90d14a177823585a8bde0e2
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-08-30 16:57:59 +0600

    Fronted URL added in Djoser

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -205,11 +205,21 @@ SIMPLE_JWT = {
    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 DJOSER = {
+    'EMAIL_FRONTEND_PROTOCOL' : config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN' : config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_SITE_NAME' : 'PhiMart',
     'SERIALIZERS': {  
         'user_create': 'users.serializers.UserCreateSerializer',
         'current_user': 'users.serializers.UserSerializer',
-
-    }
+        'password_reset': 'djoser.serializers.PasswordResetSerializer',
+        'password_reset_confirm': 'djoser.serializers.PasswordResetConfirmSerializer',
+        'set_password': 'djoser.serializers.SetPasswordSerializer',
+    },
+    'SEND_ACTIVATION_EMAIL': False,
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}/',
+    'USERNAME_RESET_EMAIL': False,
+    'ACTIVATION_URL': 'activate/{uid}/{token}/',
+    'TOKEN_MODEL': None,
 }
 
 
--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
-from .views import api_root_view
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import api_root_view
 
-
+# DRF-YASG Schema
 schema_view = get_schema_view(
    openapi.Info(
       title="Phimart - E-commerce API",
@@ -22,16 +21,26 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', api_root_view),
-    # path('api-auth/', include('rest_framework.urls')),
-    
-    path('api/v1/', include('api.urls'),name='api-root'),
+
+    # app APIs
+    path('api/v1/', include('api.urls'), name='api-root'),
+
+    # Djoser auth endpoints (register, login, logout, password reset/change)
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),  # JWT auth
+
+    # Swagger & Redoc documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    
-] + debug_toolbar_urls()
+]
 
-urlpatterns += static(settings.MEDIA_URL, documenet_root = settings.MEDIA_ROOT)
+# Debug Toolbar (only in development)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+
+# Static & Media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
