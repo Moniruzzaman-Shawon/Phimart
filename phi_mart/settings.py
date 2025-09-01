dc3119c5778a1a54a254a4fd50ac705abe6a7b57
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-09-02 00:42:22 +0600

    updated staticfiles name

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_FILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 
